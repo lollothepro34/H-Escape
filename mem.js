62e4c16cba2aafb1356ee87c386f4922ccd20a6f
FuturePaul41 <frara.alessandro@gmail.com>
2022-03-23 11:54:20 +0100

    g

--- mem.js
+++ mem.js
@@ -50,7 +50,7 @@ const generateGame = () => {
         throw new Error("The dimension of the board must be an even number.")
     }
 
-    const emojis = ['🥔', '🍒', '🥑', '🌽', '🥕', '🍇', '🍉', '🍌', '🥭', '🍍']
+    const emojis = ['🚙', '🔎', '📱', '💡', '💻', '🌱', '🌿', '🐝', '🏆', '⏳']
     const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
     const items = shuffle([...picks, ...picks])
     const cards = `
@@ -144,8 +144,5 @@ const attachEventListeners = () => {
     })
 }
 
-
-
-
-
-
+generateGame()
+attachEventListeners()
